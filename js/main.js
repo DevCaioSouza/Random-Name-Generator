769c2c567ed9c01a8d1a12d10a6d3de5e6d4325f
caio souza <caiocbahia@hotmail.com>
2022-01-28 23:35:36 -0300

    Fixing bad commits

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import {namesOne, namesTwo} from './names.js'
+
+const initApp = () => {
+    document.getElementById("submitForm")
+    .addEventListener("submit", (event) => {
+        event.preventDefault();
+        // clear out suggestions
+        // generate names
+        // display names
+    });
+}
+
+document.addEventListener("DomContentLoaded", initApp);
+
+const clearSuggestions
